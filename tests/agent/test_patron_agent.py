4ede6dc00bb567cbdf93df174786ae0303f18f72
Sergii Kirichko <sergii.kirichko@cprime.com>
2026-03-10 23:30:46 +0100

    Add patron agent with weather functionality and tests

---
+++ tests/agent/test_patron_agent.py
@@ -0,0 +1,14 @@
+import pytest
+
+from agents.patron_itself.patron_agent import agent
+
+
+@pytest.mark.asyncio
+async def test_agent():
+    user_ask = "It's cold today"
+
+    response = agent.invoke(
+        {"messages": [{"role": "user", "content": user_ask}]}
+    )
+
+    print(response)
